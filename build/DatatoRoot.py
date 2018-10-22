fa75214a7ae17e24b73718fb947c3b1bc8677a79
Alan Horst <ahorst@terpmail.umd.edu>
2018-10-22 13:31:36 -0400

    Fixed to work more generally

--- build/DatatoRoot.py
+++ build/DatatoRoot.py
@@ -27,10 +27,11 @@ for line in lines:
 			yes = ('yes','y','ye')
 			no = ('no','n')
 			name = labels[i-2]
+			hist = str(labels[i-2])
 			choice = raw_input()
 			if choice in yes:
 				name = TGraphErrors(len(Arrx[i-1]), Arrx[i-1], Det[i-1], Err[i-1], Errs[i-1])
-				name.Write();
+				name.Write("%s" % (hist))
 			elif choice in no:
 				p = True
 			else:
@@ -44,8 +45,4 @@ for line in lines:
 		Det[i].append(float(words[2]))
 		Errs[i].append(float(words[3]))
 		Err[i].append(float(words[1]))
-#outfile.Write()
 print ('done')
-#TH2F* h3 = new TH2F("Ndetect", "Mean Detected Photons Per Muon", 30, -15, 15, , -2, 2) use this to define a 2d histogram with 20 bins in x and y axis (range -6 to 6)
-#ntuple->Draw("x:y>>Ndetect","det","SURF3") use this to fill the histogram using the ntuple created in this .py, using 'avg' as n for the histogram, SURF3 is a draw option
-#make sure in the fill function above, the arguments (x,y,avg in this case) match the printed 'labels', otherwise the histogram will not fill correctly
