65b3b9224d457931a8d4bc5331b9a45fbc600ada
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2019-09-11 12:28:39 -0400

    New plotting scripts

---
+++ Plot/bin/MakeLYvXGraph.cc
@@ -0,0 +1,111 @@
+#include "HGCalTileSim/Tile/interface/LYSimFormat.hh"
+
+#include "UserUtils/Common/interface/ArgumentExtender.hpp"
+#include "UserUtils/Common/interface/Maths.hpp"
+#include "UserUtils/Common/interface/STLUtils/StringUtils.hpp"
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TGraphErrors.h"
+
+int
+main( int argc, char** argv )
+{
+  usr::po::options_description desc(
+    "Generating TGraphs For a certain selection criteria" );
+  desc.add_options()
+    ( "beamy,y", usr::po::defvalue<double>( 0 ), "Fixed value of y" )
+    ( "dimplerad,r", usr::po::multivalue<double>(),
+    "List of different dimple radius, different dimple radii will be saved to "
+    "different plot" )
+    ( "dimpleind,d", usr::po::multivalue<double>(),
+    "List of different dimple indents, different indent values will be saved "
+    "to different plot" )
+    ( "inputfiles,f", usr::po::multivalue<double>(),
+    "List of input root files" )
+    ( "output,o", usr::po::defvalue<std::string>(
+    (usr::resultpath( "HGCalTileSim", "Plot" )/"LYvX.root").string() ),
+    "Output root file" )
+  ;
+
+  usr::ArgumentExtender args;
+  args.AddOptions( desc );
+  args.ParseOptions( argc, argv );
+
+  const std::string output = args.Arg<std::string>( "output" );
+  const double y           = args.Arg<double>( "beamy" );
+  const std::vector<double> rlist
+    = args.Arg<std::vector<double> >( "dimplerad" );
+  const std::vector<double> dlist
+    = args.Arg<std::vector<double> >( "dimpleind" );
+  const std::vector<std::string> filelist
+    = args.Arg<std::vector<std::string> >( "inputfiles" );
+
+  // Loading input files
+  TChain tree( "LYSim", "LYSim" );
+
+  for( const auto file : filelist ){
+    tree.Add( file.c_str() );
+  }
+
+  typedef std::map<double, std::vector<double> > SinglePointResult;
+  typedef std::map<double, SinglePointResult>    R_Result;
+  typedef std::map<double, R_Result>             Results;
+  Results results;
+
+  LYSimFormat format;
+  format.LoadBranches( &tree );
+  double width = 0 ;
+
+  // Creating the List of results in memory
+  for( int i = 0; i < tree.GetEntries(); ++i ){
+    tree.GetEntry( i );
+
+    if( format.beam_center_y != y ){continue;}
+
+    for( const auto r : rlist ){
+      if( format.dimple_radius != r ){ continue; }
+
+      for( const auto d : dlist ){
+        if( format.dimple_indent != d ){ continue; }
+        results[r][d][format.beam_center_x].push_back( format.nphotons );
+        if( i != 0 && format.beam_width != width ){
+          std::cout << "Warning! Different beam width detected! "
+                       "Make sure this is what you want!" << std::endl;
+        }
+        width = format.beam_width;
+      }
+    }
+  }
+
+  // Creating a single TGraph for each r/d results
+  TFile* file = TFile::Open( output.c_str(), "RECREATE" );
+
+  for( const auto r : rlist ){
+    for( const auto d : dlist ){
+      const std::string graphname = usr::fstr( "R%.1lf_D%.1lf", r, d );
+      std::vector<double> x_list;
+      std::vector<double> v_list;
+      std::vector<double> unc_list;
+      std::vector<double> w_list;
+
+      for( const auto p : results[r][d] ){
+        x_list.push_back( p.first );
+        v_list.push_back( usr::Mean( p.second ) );
+        unc_list.push_back( usr::StdDev( p.second ) );
+        w_list.push_back( width );
+      }
+
+      TGraphErrors g( x_list.size(),
+                      x_list.data(), v_list.data(),
+                      w_list.data(), unc_list.data() );
+      g.SetName( graphname.c_str() );
+      g.Write();
+    }
+  }
+
+  file->Close();
+
+
+  return 0;
+}
---
+++ Plot/bin/PlotLYvXGraph.cc
@@ -0,0 +1,83 @@
+#include "UserUtils/Common/interface/ArgumentExtender.hpp"
+#include "UserUtils/Common/interface/STLUtils/Filesystem.hpp"
+#include "UserUtils/PlotUtils/interface/Simple1DCanvas.hpp"
+
+#include "TFile.h"
+
+int
+main( int argc, char** argv )
+{
+  usr::po::options_description desc(
+    "Generating TGraphs For a certain selection criteria" );
+  desc.add_options()
+    ( "dimplerad,r", usr::po::multivalue<double>(),
+    "List of different dimple radius to include in the plot" )
+    ( "dimpleind,d", usr::po::multivalue<double>(),
+    "List of different dimple indents to include in the plot" )
+    ( "inputfile,f", usr::po::value<std::string>(),
+    "File containing the generated files" )
+    ( "output,o", usr::po::defvalue<std::string>(
+    (usr::resultpath( "HGCalTileSim", "Plot" )/"LYvX.pdf").string() ),
+    "Output plot file" )
+  ;
+
+  usr::ArgumentExtender args;
+  args.AddOptions( desc );
+  args.ParseOptions( argc, argv );
+
+  const std::vector<double> rlist
+    = args.Arg<std::vector<double> >( "dimplerad" );
+  const std::vector<double> dlist
+    = args.Arg<std::vector<double> >( "dimpleind" );
+
+  const std::vector<int> colorlist = {
+    usr::plt::col::black,
+    usr::plt::col::darkblue,
+    usr::plt::col::darkred,
+    usr::plt::col::darkgreen,
+    usr::plt::col::darkorange,
+    usr::plt::col::darkviolet
+  };
+
+  const std::vector<int> filllist = {
+    usr::plt::col::gray,
+    usr::plt::col::lightblue,
+    usr::plt::col::pink,
+    usr::plt::col::lightgreen,
+    usr::plt::col::lightgoldenrodyellow,
+    usr::plt::col::palevioletred
+  };
+
+  TFile file( args.Arg<std::string>( "inputfile" ).c_str() );
+
+  usr::plt::Simple1DCanvas c;
+  TH1D dummyhist( "", "", 1, -2.0, +2.0 );// dummy for setting up plot x range
+  c.PlotHist( dummyhist );
+
+  unsigned idx = 0;
+  double width = 0;
+
+  for( const auto r : rlist ){
+    for( const auto d : dlist ){
+      const std::string graphname = usr::fstr( "R%.1lf_D%.1lf", r, d );
+      TGraph* graph               = (TGraph*)( file.Get( graphname.c_str() ) );
+      c.PlotGraph( graph,
+        usr::plt::PlotType( usr::plt::fittedfunc ),
+        usr::plt::LineColor( colorlist[idx] ),
+        usr::plt::FillColor( filllist[idx] ),
+        usr::plt::FillStyle( usr::plt::sty::fillsolid ),
+        usr::plt::EntryText( usr::fstr( "R=%.1lfmm D=%.1lf", r, d ) )
+        );
+      width = graph->GetErrorX( 0 );
+      ++idx;
+    }
+  }
+
+  c.Pad().Xaxis().SetTitle( "Beam Center X [mm]" );
+  c.Pad().Yaxis().SetTitle( "Detected Photons" );
+
+  c.DrawLuminosity( usr::fstr( "Trigger Width: %.1lf", width ) );
+  c.DrawCMSLabel( "Simulation", "HGCal" );
+  c.SaveAsPDF( args.Arg<std::string>( "output" ) );
+
+}
